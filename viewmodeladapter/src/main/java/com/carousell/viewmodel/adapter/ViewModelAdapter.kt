8b28413bac549acd63c2d3b9f8c9cb4c8e72dc4f
Jintin <jintin.lin1018@gmail.com>
2020-07-03 18:45:36 +0800

    udpate

--- viewmodeladapter/src/main/java/com/carousell/viewmodel/adapter/ViewModelAdapter.kt
+++ viewmodeladapter/src/main/java/com/carousell/viewmodel/adapter/ViewModelAdapter.kt
@@ -21,17 +21,15 @@ abstract class ViewModelAdapter(callback: DiffUtil.ItemCallback<ItemViewModel> =
         holder.unbind()
     }
 
-    private fun getModelTypeInt(clz: Class<out ItemViewModel>): Int {
+    final override fun getItemViewType(position: Int): Int {
+        val clz = getItem(position).javaClass
+        
         return modelTypeMapping[clz] ?: run {
             val index = modelTypeMapping.size
             modelTypeMapping[clz] = index
             typeModelMapping[index] = clz
             return@run index
         }
-    }
-
-    final override fun getItemViewType(position: Int): Int {
-        return getModelTypeInt(getItem(position).javaClass)
     }
 
     final override fun onCreateViewHolder(
