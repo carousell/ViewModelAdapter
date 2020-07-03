2ca5622d6bedd412df4b7c3990b911dc751dcfcb
Jintin <jintin.lin1018@gmail.com>
2020-07-03 18:42:37 +0800

    refactor adapter

--- viewmodeladapter/src/main/java/com/carousell/viewmodel/adapter/ViewModelAdapter.kt
+++ viewmodeladapter/src/main/java/com/carousell/viewmodel/adapter/ViewModelAdapter.kt
@@ -1,5 +1,6 @@
 package com.carousell.viewmodel.adapter
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 
@@ -9,6 +10,9 @@ import androidx.recyclerview.widget.ListAdapter
 abstract class ViewModelAdapter(callback: DiffUtil.ItemCallback<ItemViewModel> = ItemViewModel.ItemDiffCallback()) :
     ListAdapter<ItemViewModel, ViewModelHolder<out ItemViewModel>>(callback) {
 
+    private val modelTypeMapping = mutableMapOf<Class<out ItemViewModel>, Int>()
+    private val typeModelMapping = mutableMapOf<Int, Class<out ItemViewModel>>()
+
     override fun onBindViewHolder(holder: ViewModelHolder<out ItemViewModel>, position: Int) {
         holder.bind(getItem(position))
     }
@@ -16,4 +20,31 @@ abstract class ViewModelAdapter(callback: DiffUtil.ItemCallback<ItemViewModel> =
     override fun onViewRecycled(holder: ViewModelHolder<out ItemViewModel>) {
         holder.unbind()
     }
+
+    private fun getModelTypeInt(clz: Class<out ItemViewModel>): Int {
+        return modelTypeMapping[clz] ?: run {
+            val index = modelTypeMapping.size
+            modelTypeMapping[clz] = index
+            typeModelMapping[index] = clz
+            return@run index
+        }
+    }
+
+    final override fun getItemViewType(position: Int): Int {
+        return getModelTypeInt(getItem(position).javaClass)
+    }
+
+    final override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ViewModelHolder<out ItemViewModel> {
+        return typeModelMapping[viewType]?.let {
+            onCreateViewHolder(parent, it)
+        } ?: throw RuntimeException("No support onCreateViewHolder")
+    }
+
+    abstract fun onCreateViewHolder(
+        parent: ViewGroup,
+        modelType: Class<out ItemViewModel>
+    ): ViewModelHolder<out ItemViewModel>
 }
